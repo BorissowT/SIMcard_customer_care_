f3b82795e931c65d03cf28e0c0d3fb938a9b31e7
Timofej <timsregisters@gmail.com>
2020-04-08 14:25:33 +0300

    setlocale

--- SIMcard_customer_care/SIMcard_customer_care.cpp
+++ SIMcard_customer_care/SIMcard_customer_care.cpp
@@ -3,11 +3,13 @@
 #include <stdlib.h>
 #include<iomanip> 
 #include "Customer.h"
-
+#include <Windows.h>
 
 
 int main()
 {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
 	setlocale(LC_ALL, "rus");
 	Tree* head = new Tree;
 	head = NULL;
